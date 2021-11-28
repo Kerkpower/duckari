248eafed1df27e818a8252cfe51eea2cb9c5b06e
thomm.o <olijdthompson@gmail.com>
2021-11-28 15:03:30 +0000

    Implement ability to register event listeners

--- duckari/duck.py
+++ duckari/duck.py
@@ -1,8 +1,11 @@
+import collections
 import typing as t
 
 import hikari
 
 __all__ = ["Duck"]
+
+_ListenerT = t.Callable[[hikari.Event], t.Coroutine[t.Any, t.Any, None]]
 
 
 class Duck:
@@ -13,6 +16,14 @@ class Duck:
         self._intents: hikari.Intents = intents
         self._hikari_bot: t.Optional[hikari.GatewayBot] = None
         self._extra_kwargs: t.Dict[str, t.Any] = kwargs
+        self._listeners: t.Dict[t.Type[hikari.Event], t.List[_ListenerT]] = collections.defaultdict(list)
+
+    def listen(self, event: t.Type[hikari.Event]) -> t.Callable[[_ListenerT], _ListenerT]:
+        def decorate(func: _ListenerT) -> _ListenerT:
+            self._listeners[event].append(func)
+            return func
+
+        return decorate
 
     async def on_message_create(self, event: hikari.MessageCreateEvent) -> None:
         assert self._prefix is not None
@@ -27,5 +38,9 @@ class Duck:
 
         if self._prefix is not None:
             self._hikari_bot.subscribe(hikari.MessageCreateEvent, self.on_message_create)
+
+        for event, listeners in self._listeners.items():
+            for listener in listeners:
+                self._hikari_bot.subscribe(event, listener)
 
         self._hikari_bot.run(**kwargs)
